fb85de26289a4a9c33cc0929885085468ff8f586
Strizhak Roman <milon777.official@gmail.com>
2024-01-11 14:04:49 +0300

    finished cards

--- app/components/cards.tsx
+++ app/components/cards.tsx
@@ -1,8 +1,12 @@
-import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
+import Grid from "@mui/material/Unstable_Grid2";
 import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import SavingsIcon from "@mui/icons-material/Savings";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import PaymentIcon from "@mui/icons-material/Payment";
 
 export default function Cards() {
   return (
@@ -10,29 +14,93 @@ export default function Cards() {
       <Grid
         container
         spacing={3}
-        textAlign={"center"}
-        alignItems={"center"}
-        sx={{ width: "inherit", ml: 0 }}>
-        <Grid
-          item
-          xs={12}
-          sm={6}
-          md={3}
-          display={"flex"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          sx={{
-            backgroundColor: "gray",
-            padding: 0,
-          }}
-          style={{ padding: 0 }}>
-          <Box mr={1}>
-            <RocketLaunchIcon fontSize="large" />
-          </Box>
-          <Box>
-            <Typography variant="h6">Fast Delivery</Typography>
-            <Typography variant="body1">Start from $10</Typography>
-          </Box>
+        sx={{ textAlign: "center", alignItems: "center" }}>
+        <Grid xs={12} sm={6} md={3}>
+          <Paper
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              textAlign: "left",
+            }}>
+            <Box sx={{ mr: 1 }}>
+              <RocketLaunchIcon fontSize="large" sx={{ color: "#2e3748" }} />
+            </Box>
+            <Box>
+              <Typography variant="h6" color={"#2b3445"}>
+                Fast Delivery
+              </Typography>
+              <Typography variant="body1" color={"#7d879c"}>
+                Start from $10
+              </Typography>
+            </Box>
+          </Paper>
+        </Grid>
+
+        <Grid xs={12} sm={6} md={3}>
+          <Paper
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              textAlign: "left",
+            }}>
+            <Box sx={{ mr: 1 }}>
+              <SavingsIcon fontSize="large" sx={{ color: "#2e3748" }} />
+            </Box>
+            <Box>
+              <Typography variant="h6" color={"#2b3445"}>
+                Money Guarantee
+              </Typography>
+              <Typography variant="body1" color={"#7d879c"}>
+                7 Days Back
+              </Typography>
+            </Box>
+          </Paper>
+        </Grid>
+
+        <Grid xs={12} sm={6} md={3}>
+          <Paper
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              textAlign: "left",
+            }}>
+            <Box sx={{ mr: 1 }}>
+              <AlarmIcon fontSize="large" sx={{ color: "#2e3748" }} />
+            </Box>
+            <Box>
+              <Typography variant="h6" color={"#2b3445"}>
+                365 Days
+              </Typography>
+              <Typography variant="body1" color={"#7d879c"}>
+                For free return
+              </Typography>
+            </Box>
+          </Paper>
+        </Grid>
+
+        <Grid xs={12} sm={6} md={3}>
+          <Paper
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              textAlign: "left",
+            }}>
+            <Box sx={{ mr: 1 }}>
+              <PaymentIcon fontSize="large" sx={{ color: "#2e3748" }} />
+            </Box>
+            <Box>
+              <Typography variant="h6" color={"#2b3445"}>
+                Payment
+              </Typography>
+              <Typography variant="body1" color={"#7d879c"}>
+                Secure system
+              </Typography>
+            </Box>
+          </Paper>
         </Grid>
       </Grid>
     </Container>
